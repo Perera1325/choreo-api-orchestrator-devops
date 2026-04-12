70ca10fb12d9d4d3ce3283abacd72044a577abeb
Vinod Ravindu Perera <bamunuge2002@gmail.com>
2026-04-12 22:57:26 +0530

    add user-service

---
+++ services/user-service/app.js
@@ -0,0 +1,15 @@
+echo "const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('User Service Running');
+});
+
+app.get('/user', (req, res) => {
+  res.json({ id: 1, name: 'Vinod' });
+});
+
+app.listen(PORT, () => {
+  console.log(\`User Service running on port \${PORT}\`);
+});" > app.js
