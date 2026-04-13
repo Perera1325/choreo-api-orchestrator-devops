037942fafc3ecdc11ff74f7a4bb56c11c68c3ecf
Vinod Perera <165481612+Perera1325@users.noreply.github.com>
2026-04-13 09:15:01 +0530

    Change single quotes to double quotes in app.js

--- services/user-service/app.js
+++ services/user-service/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('User Service Running');
+app.get("/", (req, res) => {
+  res.send("User Service Running");
 });
 
-app.get('/user', (req, res) => {
-  res.json({ id: 1, name: 'Vinod' });
+app.get("/user", (req, res) => {
+  res.json({ id: 1, name: "Vinod" });
 });
 
 app.listen(PORT, () => {
-  console.log(`User Service running on port ${PORT}`);
+  console.log("User Service running on port " + PORT);
 });
